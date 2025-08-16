e1e471f42060da0250372919cd89cb53cff06965
fridfn <faridfathonin@gmail.com>
2025-08-16 18:59:01 +0700

    minor update

--- utils/generateKey.js
+++ utils/generateKey.js
@@ -2,7 +2,7 @@ export const generateKey = (withTime = false) => {
   const date = new Date();
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0")
-  const day = String(date.getDay()).padStart(2, "0")
+  const day = String(date.getDate()).padStart(2, "0")
   
   if (!withTime) return `${year}${month}${day}`
   
