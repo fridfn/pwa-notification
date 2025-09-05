987415e7adbc7f2ead3a61afcba5ae399f3a3646
fridfn <faridfathonin@gmail.com>
2025-09-05 22:12:37 +0700

    menambahkan halaman antarmuka untuk endpoin /

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+import cors from 'cors';
+import path from "path";
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 
 import saveSubscription from './api/saveSubscription.js';
 import sendNotification from './api/sendNotification.js';
@@ -11,9 +12,15 @@ import registerUser from './api/registerUser.js';
 import DailyActivity from './api/DailyActivity.js';
 
 const app = express();
+const __dirname = path.resolve();
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "public/src"))
 
 app.post('/api/saveSubscription', saveSubscription);
 app.post('/api/sendNotification', sendNotification);
@@ -22,6 +29,11 @@ app.post('/api/saveFeedback', saveFeedback);
 app.post('/api/checkemail', checkEmail);
 app.post('/api/user/register', registerUser);
 app.post('/api/user/activity', DailyActivity);
+
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Endpoint - Farid Fathoni N" })
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server jalan di http://localhost:${PORT}`));
